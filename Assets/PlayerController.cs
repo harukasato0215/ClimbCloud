80389a3ef34a7fc1aad0134edb983c6ff1f45179
harukasato0215 <popo02150125@gmail.com>
2024-05-09 18:00:05 +0900

    映像

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
+    Animator animator;
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     {
         Application.targetFrameRate = 60;
         this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -40,7 +42,9 @@ public class PlayerController : MonoBehaviour
         if (key != 0)
         {
             transform.localScale = new Vector3(key, 1, 1);
+
         }
 
+        this.animator.speed = speedx / 2.0f;
     }
 }
